45d2cf345edf45a0a007be993e911bb40c1127f7
Lars Dietrich <lars@dietrich-hosting.de>
2019-12-20 20:28:36 +0100

    Make it possible to set external fan table file

--- src/common/fanTable.ts
+++ src/common/fanTable.ts
@@ -28,13 +28,23 @@ export class TemperaturEntry
  */
 export function readFanTables(): FanTable[]
 {
-    let fanTablesFilePath: string = "../data/fantables.json";
     let fs: any = Environment.getObject("fs");
     let path: any = Environment.getObject("path");
+    let fanTablesFilePath: string = "../data/fantables.json";
+    fanTablesFilePath = path.join(__dirname, fanTablesFilePath);
+    if (process.env.TUXEDO_FAN_TABLE_FILE) {
+        fs.writeFileSync(
+            System.LOGFILE_PATH,
+            new Date().toISOString() + ' use TUXEDO_FAN_TABLE_FILE: ' + process.env.TUXEDO_FAN_TABLE_FILE + '\n',
+            {flag: "a"}
+        );
+        fanTablesFilePath = process.env.TUXEDO_FAN_TABLE_FILE;
+    }
+
     let tables = new Array<FanTable>();
 
     fs.writeFileSync(System.LOGFILE_PATH, new Date().toISOString() + " Read fan table\n", { flag: "a" });
-    let content = fs.readFileSync(path.join(__dirname, fanTablesFilePath)).toString()
+    let content: string = fs.readFileSync(fanTablesFilePath).toString();
 
     tables = JSON.parse(content);
     return tables;
